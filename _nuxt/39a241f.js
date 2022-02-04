82466559281bf635c9272076cb428f1c80de408a
pongvarid <pongvarid@gmail.com>
2022-02-04 18:36:47 +0700

    Updates

--- _nuxt/39a241f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{810:function(n,t,e){"use strict";e.r(t);var c={},l=e(49),component=Object(l.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("div",[n._v("\n    historyaccount\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
--- _nuxt/7a8a2f2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{794:function(t,e,n){"use strict";n.r(e);var o={layout:"home"},l=n(49),component=Object(l.a)(o,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("Home-Contract")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
