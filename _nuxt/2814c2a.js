42c1f70679031ef49eed1fdf480e71550cf7ca5d
pongvarid <pongvarid@gmail.com>
2022-02-04 15:34:37 +0700

    Updates

--- _nuxt/2814c2a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[64],{786:function(n,t,e){"use strict";e.r(t);var l={},c=e(50),component=Object(c.a)(l,(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Signals-ListSignals")],1)}),[],!1,null,null,null);t.default=component.exports}}]);
