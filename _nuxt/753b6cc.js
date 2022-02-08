6e08b9d08aa428af08b5d859adcfc000e68a7997
pongvarid <pongvarid@gmail.com>
2022-02-08 15:38:42 +0700

    Updates

--- _nuxt/753b6cc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{751:function(t,n,e){"use strict";e.r(n);var c={watch:{}},o=e(47),component=Object(o.a)(c,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("HistoryAccount-ListHistory")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/7e8a231.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{768:function(e,n,t){"use strict";t.r(n);var c={},r=t(47),component=Object(r.a)(c,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("Ecommerce-Brand")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
