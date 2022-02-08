12b343218f793a065284cda7e66af58496924120
pongvarid <pongvarid@gmail.com>
2022-02-08 18:30:10 +0700

    Updates

---
+++ _nuxt/fdcb02b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[65],{756:function(t,n,e){"use strict";e.r(n);var c={watch:{}},o=e(48),component=Object(o.a)(c,(function(){var t=this.$createElement,n=this._self._c||t;return n("div",[n("HistoryAccount-ListHistory")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
