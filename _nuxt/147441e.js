26e21ec0962b95cd0e5b8be9bd3e76a79ba96531
pongvarid <pongvarid@gmail.com>
2022-03-15 16:43:37 +0700

    Updates

---
+++ _nuxt/147441e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[91],{909:function(t,e,n){"use strict";n.r(e);var o={layout:"home"},l=n(55),component=Object(l.a)(o,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("Home-Contract")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/3d543c2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[96],{894:function(e,n,t){"use strict";t.r(n);var c={},r=t(55),component=Object(r.a)(c,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("Ecommerce-Brand")],1)}),[],!1,null,null,null);n.default=component.exports}}]);
