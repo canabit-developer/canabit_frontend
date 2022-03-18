6f8585cdace62999675126206610891c2ab012f1
pai_commits <sumer_cup@hotmail.com>
2022-03-18 17:35:20 +0700

    update:redeem items move point

--- components/Profilebrand/RedeemList.vue
+++ components/Profilebrand/RedeemList.vue
@@ -9,7 +9,6 @@
                 <div class="w-1/2">
                     <div class="flex flex-col md:flex-row">
                         <v-select @change="startup()" v-model="sortBy" :items="sorts" item-text="text" item-value="value" class="mr-2" dense outlined label="Sort By Point"></v-select>
-
                     </div>
                 </div>
             </div>
@@ -18,31 +17,34 @@
                 <v-col v-for="storeproduct,n in storeproducts" :key="n" sm="4" lg="6" md="8" xl="3">
                     <vs-card>
                         <template #title>
-                            <h3>{{storeproduct.name}}</h3>
+                            <div class="flex justify-between ">
+                                <h3 class=" text-2xl mt-1">{{storeproduct.name}}</h3>
+                                <vs-button color="#4ade80">
+                                    {{storeproduct.point_use}} Point
+                                </vs-button>
+                            </div>
+                            
                         </template>
                         <template #img>
                             <div class="h-64 w-full ">
                                 <img :src="$url+storeproduct.image" alt="" />
                             </div>
-
                         </template>
                         <template #text>
                             <div class="h-20">
-                                <p class="textellipsis">
+                                <p class="textellipsis mt-1">
                                     {{storeproduct.detail}}
                                 </p>
                             </div>
-                            <vs-button floating v-if="point.current >= storeproduct.point_use"  block color="#4ade80" class="btn-chat mt-5" @click="postRedeem(storeproduct)">
+                            <vs-button floating v-if="point.current >= storeproduct.point_use" block color="#4ade80" class="btn-chat mt-5" @click="postRedeem(storeproduct)">
                                 Redeem
                             </vs-button>
-                          <vs-button  v-else floating block disabled color="#425466" class="btn-chat mt-5"  >
-                             Your point not enough
-                          </vs-button>
+                            <vs-button v-else floating block disabled color="#425466" class="btn-chat mt-5">
+                                Your point not enough
+                            </vs-button>
                         </template>
                         <template #interactions>
-                            <vs-button color="#4ade80" >
-                                {{storeproduct.point_use}} Point
-                            </vs-button>
+
                         </template>
                     </vs-card>
                 </v-col>
@@ -73,10 +75,15 @@ export default {
         storeproducts: [],
         response: false,
         form: {},
-        sortBy:'asc',
-        sorts:[
-          {'text':'Descending',value:'asc'},
-          {'text':'Ascending',value:'desc'},
+        sortBy: 'asc',
+        sorts: [{
+                'text': 'Descending',
+                value: 'asc'
+            },
+            {
+                'text': 'Ascending',
+                value: 'desc'
+            },
         ]
     }),
     async created() {
@@ -86,7 +93,7 @@ export default {
     methods: {
         async startup() {
             this.storeproducts = await Core.getHttp('/api/store/product/?is_active=true')
-            this.storeproducts = _.orderBy(this.storeproducts,['point_use'],[this.sortBy])
+            this.storeproducts = _.orderBy(this.storeproducts, ['point_use'], [this.sortBy])
         },
         async getRedeem() {
             let check = await Web.confirm('Thank you for the Redeem')
@@ -115,21 +122,21 @@ export default {
         imageStoreProduct() {
             return Store.image
         },
-      user: () => {
-        return Auth.user
-      },
-      point: () => {
-        return Auth.point
-      },
-      tier: () => {
-        return Auth.tier
-      },
-      tiers: () => {
-        return Auth.tiers
-      },
-      setting: () => {
-        return Auth.setting
-      },
+        user: () => {
+            return Auth.user
+        },
+        point: () => {
+            return Auth.point
+        },
+        tier: () => {
+            return Auth.tier
+        },
+        tiers: () => {
+            return Auth.tiers
+        },
+        setting: () => {
+            return Auth.setting
+        },
 
     }
 }
