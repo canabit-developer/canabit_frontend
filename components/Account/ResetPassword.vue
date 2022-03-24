bc1ff4f9da069316340169750770e58db8595c18
pai_commits <sumer_cup@hotmail.com>
2022-03-24 11:32:27 +0700

    update: issue passwords dont match

--- components/Account/ResetPassword.vue
+++ components/Account/ResetPassword.vue
@@ -1,34 +1,36 @@
 <template>
 <div>
-    <div class="px-3"> 
+    <div class="px-3">
         <v-card-text class="pt-5" v-if="(user.register_by=='Register')|| (user.register_by == null)|| (user.register_by == 'Web')">
             <v-row>
-                <v-alert class="mt-3" outlined dense  type="info" prominent border="left" >
-                     Password must contain at least 8 uppercase letters, lowercase letters and numbers.
+                <v-alert class="mt-3" outlined dense type="info" prominent border="left">
+                    Password must contain at least 8 uppercase letters, lowercase letters and numbers.
                 </v-alert>
-                <v-col cols="12" sm="8" md="8" class="-mt-3"> 
+                <v-col cols="12" sm="8" md="8" class="-mt-3">
                     <div v-if="error.old_password">
                         <v-alert dense outlined text v-for="(message,i) in error.old_password" :key="i" type="error" :value="true">
                             {{message}}
                         </v-alert>
                     </div>
-                    <div v-if="error.password">
-                        <v-alert dense outlined text v-for="(message,i) in error.password" :key="i" type="error" :value="true">
-                            {{message}}
-                        </v-alert>
-                    </div>
-                    <div v-if="error.password_confirm">
-                        <v-alert dense outlined text v-for="(message,i) in error.password_confirm" :key="i" type="error" :value="true">
-                            {{message}}
-                        </v-alert>
-                    </div>
+                    <v-col cols="12" md="6">
+                        <div v-if="error.password">
+                            <v-alert dense outlined text v-for="(message,i) in error.password" :key="i" type="error" :value="true">
+                                {{message}}
+                            </v-alert>
+                        </div>
+                        <div v-if="error.password_confirm">
+                            <v-alert dense outlined text v-for="(message,i) in error.password_confirm" :key="i" type="error" :value="true">
+                                {{message}}
+                            </v-alert>
+                        </div>
+                    </v-col>
                     <form @submit.prevent="changePassword()" ref="formResetPassword">
                         <h2 class="text-xl mt-4 mb-4">Change Password</h2>
-                        <v-text-field  :type="isPasswordVisible ? 'text' : 'password'" :append-icon="isPasswordVisible ? `mdi-eye-off-outline` : `mdi-eye-outline`" @click:append="isPasswordVisible = !isPasswordVisible"  required class="w-full md:w-1/2 m-1" hint="*  Please Enter Old Password " v-model="form.old_password" prepend-inner-icon="em em-lock" label="Old Password" dense outlined></v-text-field>
+                        <v-text-field :type="isPasswordVisible ? 'text' : 'password'" :append-icon="isPasswordVisible ? `mdi-eye-off-outline` : `mdi-eye-outline`" @click:append="isPasswordVisible = !isPasswordVisible" required class="w-full md:w-1/2 m-1" hint="*  Please Enter Old Password " v-model="form.old_password" prepend-inner-icon="em em-lock" label="Old Password" dense outlined></v-text-field>
                         <v-text-field :type="isPasswordVisible ? 'text' : 'password'" :append-icon="isPasswordVisible ? `mdi-eye-off-outline` : `mdi-eye-outline`" @click:append="isPasswordVisible = !isPasswordVisible" required class="w-full md:w-1/2 m-1" hint="*  Please Enter New Password " v-model="form.password" prepend-inner-icon="em em-closed_lock_with_key" label="New Password" dense outlined></v-text-field>
                         <v-text-field :type="isPasswordVisible ? 'text' : 'password'" :append-icon="isPasswordVisible ? `mdi-eye-off-outline` : `mdi-eye-outline`" @click:append="isPasswordVisible = !isPasswordVisible" required class="w-full md:w-1/2 m-1" hint="*  Please Enter New Password " v-model="form.password_confirm" prepend-inner-icon="em em-closed_lock_with_key" label="New Password Confirm" dense outlined></v-text-field>
                         <vs-button floating type="submit" color="success">Update Password</vs-button>
-                    </form> 
+                    </form>
                 </v-col>
 
                 <v-col cols="12" sm="2" md="4" class="d-none d-sm-flex justify-center position-relative">
@@ -38,7 +40,7 @@
         </v-card-text>
         <v-card-text v-else>
             <div class="p-6">
-                <v-alert    outlined text type="info" :value="true">
+                <v-alert outlined text type="info" :value="true">
                     You are logged in with a social network, you cannot change your password.
                 </v-alert>
             </div>
