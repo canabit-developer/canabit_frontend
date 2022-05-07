eaf3a8f533f60bed1aa39839dd5f294bb8ac746e
pongvarid <pongvarid@gmail.com>
2022-05-07 11:27:00 +0700

    btn lang

--- components/Core/LanguageSwitch.vue
+++ components/Core/LanguageSwitch.vue
@@ -1,8 +1,10 @@
 <template>
 <div>
     <v-select v-if="selected" @change="switchLang" v-model="lang" :items="availableLocales" item-text="name" item-value="code" label="ภาษา"></v-select>
-    <nuxt-link v-else v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)"><span :class="(lang == locale.code)?'text-gray-600':'text-green-300'">{{ locale.name }}</span> &nbsp;</nuxt-link>
-    
+    <div v-else  class="flex">
+        <v-btn @click="switchLangClick(locale.code)" color="success" text  v-for="locale in availableLocales" :key="locale.code"><span :class="(lang == locale.code)?'text-gray-600':'font-semibold text-green-500'">{{ locale.name }}</span></v-btn>
+    </div>
+     
 </div>
 </template>
 
@@ -25,14 +27,23 @@ export default {
             return this.$i18n.locale
         }
     },
-    async created() {
+    async created() { 
+        
         this.lang = this.currentLang
         console.log(this.$i18n);
     },
     methods: {
-        async switchLang() {
-            await this.$i18n.setLocale(this.lang)
+        async switchLang() { 
+            await this.$i18n.setLocale(this.lang)  
+              this.lang = this.currentLang
+        },
+
+        async switchLangClick(lang){
+           await this.$i18n.setLocale(lang) 
+            this.lang = this.currentLang
+   
         }
+      
     }
 }
 </script>
